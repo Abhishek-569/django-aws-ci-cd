534305d533bad902863b14be844e781729cee6b6
Abhishek-569 <akabhishek4444@gmail.com>
2022-09-27 22:18:58 +0530

    first comm

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>our mind blowing home page by Akshay Kumar</h1>"""
+   text = """<h1>our mind blowing home page by Aman Raj</h1>"""
    return HttpResponse(text)
